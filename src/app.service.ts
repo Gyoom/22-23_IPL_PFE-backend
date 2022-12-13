bfae144fbb9b4412130afbd037b72403b855cd93
Ciara Van Muysewinkel <ciara.vanmuysewinkel@student.vinci.be>
2022-12-13 22:15:38 +0100

    [MODIFY] Patch of issues

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Neo4jService } from 'nest-neo4j/src/neo4j.service';
+import { Neo4jService } from 'nest-neo4j/dist';
 
 @Injectable()
 export class AppService {
